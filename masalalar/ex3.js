d857fe6e03907bf82e0c9650edc2c5cd94ed8e38
Zokirkhon <zokirxonkotibxanov@gmail.com>
2022-09-29 02:16:49 +0900

    added some methods

--- masalalar/ex3.js
+++ masalalar/ex3.js
@@ -1,6 +1,25 @@
 const ex1 = (/** @type {number[]} */ arr) =>
   arr.filter((/** @type {number} */ i) => i % 2 == 0).length;
 
+const ex2 = (/** @type {number[]} */ arr) =>
+  arr.filter((/** @type {number} */ i) => i % 2 == 1).length;
+
+
+
+
+// 3-exercise
+// 1-way // tartib bilan
+const ex3 = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+   if (arr[i]+1 !== arr[++i]) return false;
+  };
+  return true;
+};
+
+
+
 module.exports = {
   ex1,
+  ex2,
+  ex3
 };
--- tests/ex3.test.js
+++ tests/ex3.test.js
@@ -1,30 +1,74 @@
 // @ts-nocheck
-const { ex1 } = require("../masalalar/ex3");
+const { ex1, ex2, ex3 } = require("../masalalar/ex3");
 
 describe(`
 Write a JavaScript program to find the number of even digits in a an array of integers
-`, ()=> {
-    it("should return 4 if array has for even numbers and\n\
-    should not return 5,3, ...etc", ()=> {
-        let tempArr = [1, 2, 3, 4, 5, 6, 7, 8];
-        expect(ex1(tempArr)).toBe(4)
-        expect(ex1(tempArr)).not.toBe(5)
-        expect(ex1(tempArr)).not.toBe(3)
-    })
+`, () => {
+  it("should return 4 if array has for even numbers", () => {
+    let tempArr = [1, 2, 3, 4, 5, 6, 7, 8];
+    expect(ex1(tempArr)).toBe(4);
+    expect(ex1(tempArr)).not.toBe(5);
+    expect(ex1(tempArr)).not.toBe(3);
+  });
 
-    it("should return 2 if array has two even numbers", ()=> {
-        let tempArr = [1, 1, 3, 4, 5, 6, 7, 3];
-        expect(ex1(tempArr)).toEqual(2)
-        expect(ex1(tempArr)).not.toEqual(4)
-        expect(ex1(tempArr)).not.toEqual(3)
-    })
+  it("should return 2 if array has two even numbers", () => {
+    let tempArr = [1, 1, 3, 4, 5, 6, 7, 3];
+    expect(ex1(tempArr)).toEqual(2);
+    expect(ex1(tempArr)).not.toEqual(4);
+    expect(ex1(tempArr)).not.toEqual(3);
+  });
 
-    it("should return 0 if array has not even numbers", ()=> {
-        let tempArr = [1, 1, 3, 5, 5, 9, 7, 3];
-        expect(ex1(tempArr)).toEqual(0)
-        expect(ex1(tempArr)).not.toEqual(1)
-        expect(ex1(tempArr)).not.toEqual(2)
-    })
-})
+  it("should return 0 if array has not even numbers", () => {
+    let tempArr = [1, 1, 3, 5, 5, 9, 7, 3];
+    expect(ex1(tempArr)).toEqual(0);
+    expect(ex1(tempArr)).not.toEqual(1);
+    expect(ex1(tempArr)).not.toEqual(2);
+  });
+});
 
+describe(`
+Write a JavaScript program to find the number of odd digits in a an array of integers
+`, () => {
+  it("should return number of odd numbers as integer", () => {
+    let tempArr = [1, 2, 3, 4, 5, 6, 7, 8];
+    expect(ex2(tempArr)).toBe(4);
+    expect(ex2(tempArr)).not.toBe(3);
+    expect(ex2(tempArr)).not.toBe(5);
+  });
+});
 
+describe(`
+Write a JavaScript program to check whether a given array of integers is sorted in ascending
+order
+`, () => {
+  it("should return false if the array of integers is not sorted", () => {
+    let tempArr = [5, 4, 6, 3, 7, 2, 8];
+    expect(ex3(tempArr)).toBe(false);
+    expect(ex3(tempArr)).not.toBe(true);
+  });
+
+  it("should return false if the array of integers is not sorted", () => {
+    let tempArr = [1, 2, 3, 4, 5, 6, 8];
+    expect(ex3(tempArr)).toBeFalsy();
+    expect(ex3(tempArr)).not.toBeTruthy();
+  });
+
+  it("should return false if the array of integers is not sorted", () => {
+    let tempArr = [7, 1, 2, 3, 4, 5, 6];
+    expect(ex3(tempArr)).toEqual(false);
+    expect(ex3(tempArr)).not.toEqual(true);
+  });
+
+  it("should return true if the array of integers is sorted in ascending order", () => {
+    let tempArr = [1, 2, 3, 4, 5, 6];
+
+    expect(ex3(tempArr)).toBe(true);
+    expect(ex3(tempArr)).not.toBe(false);
+
+    expect(ex3(tempArr)).toBeTruthy();
+    expect(ex3(tempArr)).not.toBeFalsy();
+
+    expect(ex3(tempArr)).toEqual(true);
+    expect(ex3(tempArr)).not.toEqual(false);
+  });
+});
